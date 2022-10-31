d78f50b23eab9ca097fb7f1dba97c2bd84d33216
Haklifesavr <hak173129@gmail.com>
2022-11-01 01:49:36 +0500

    added static files

--- peopledb_backend/settings.py
+++ peopledb_backend/settings.py
@@ -196,7 +196,7 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-REACT_APP_DIR = os.path.join(BASE_DIR, "frontend/")
+REACT_APP_DIR = os.path.join(BASE_DIR, "frontend")
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, "build", "static"),
 ]
