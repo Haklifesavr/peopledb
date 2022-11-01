070ccf09335ee3a09c865ccc969d29508aa91125
Haklifesavr <hak173129@gmail.com>
2022-11-01 11:14:27 +0500

    changed urls

--- peopledb_backend/urls.py
+++ peopledb_backend/urls.py
@@ -18,7 +18,9 @@ from django.urls import path,include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
     path("admin/", admin.site.urls),
     path('peopledb/', include('peopledb.urls')),
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("signin/", TemplateView.as_view(template_name="index.html")),
+    path("signup/", TemplateView.as_view(template_name="index.html")),
 ]
